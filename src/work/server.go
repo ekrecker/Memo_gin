0be3805454c25b7ef7a4af452b31851c3b138fce
kwgch <eiichi.kendoruki@gmail.com>
2021-01-17 14:05:57 +0900

    Fix server.go

--- src/work/server.go
+++ src/work/server.go
@@ -3,7 +3,7 @@ package main
 import (
 	//"fmt"
 	"log"
-  //"net/http"
+  "net/http"
 
   "github.com/gin-gonic/gin"
   _ "github.com/jinzhu/gorm/dialects/mysql"
@@ -17,17 +17,21 @@ func main () {
 
 func serve () {
   router := gin.Default()
+
+  router.Static("/views", "./views")
+  router.StaticFS("/main", http.Dir("./views/static"))
+  /*
   router.LoadHTMLGlob("views/static/index.html")
-
   router.GET("/", func(c *gin.Context){
-    c.HTML(200, "index.html", gin.H{})
+    c.HTML(http.StatusOK, "/views/static/index.html", gin.H{})
   })
+  */
 
-  router.GET("./fetchAllMemos", controller.FetchAllMemos)
+  router.GET("/fetchAllMemos", controller.FetchAllMemos)
 
-  router.POST("./addMemo", controller.AddMemo)
+  router.POST("/addMemo", controller.AddMemo)
 
-  router.POST("./deleteMemo", controller.DeleteMemo)
+  router.POST("/deleteMemo", controller.DeleteMemo)
 
   if err := router.Run(":8080"); err != nil {
     log.Fatal("server Run Failed.: ", err)
